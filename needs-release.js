4c076aef3913f46e809be4e48981196c694a4946
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-17 00:34:13 +0200

    Output some data of to-be-released repos

--- needs-release.js
+++ needs-release.js
@@ -5,20 +5,30 @@ var cp = require('child_process')
 // var path = require('path')
 
 var result = process.argv.slice(2).map(function (arg) {
-  console.log(arg)
   try {
     var describe = cp.execSync('git describe', {
       cwd: arg,
       encoding: 'utf-8'
     })
       // If the release-tag is not the latest commit, the pattern does not match
-    if (describe.match(/-\d+-\w*/)) {
-        return { "dir": arg, "describe": describe }
+      var match = describe.match(/(.*)-(\d+)-g(\w*)/);
+      if (match) {
+        return { "dir": arg, "describeMatch": match }
     }
   } catch (e) {
   }
 }).filter(function(a) {
     return a
+}).map(function(repo) {
+    return {
+        dir: repo.dir,
+        tag: repo.describeMatch,
+        log: cp.execSync('git log --boundary --oneline '+repo.describeMatch[1] + '..',{
+            cwd: repo.dir,
+            encoding: "utf-8"
+        })
+    }
+}).forEach(function(repo) {
+    console.log(repo.dir + ": "+repo.tag[1]+"\n\n"+repo.log+"\n\n-------------------------\n")
 })
 
-console.log(result)
